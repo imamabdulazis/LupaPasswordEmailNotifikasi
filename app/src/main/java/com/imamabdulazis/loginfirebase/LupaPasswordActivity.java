4ecd42d7c36658b1c084756421e647e9c6bbd14e
imun <imamabdulazis028@gmail.com>
2019-05-23 13:00:11 +0700

    Fix program

--- app/src/main/java/com/imamabdulazis/loginfirebase/LupaPasswordActivity.java
+++ app/src/main/java/com/imamabdulazis/loginfirebase/LupaPasswordActivity.java
@@ -1,9 +1,12 @@
 package com.imamabdulazis.loginfirebase;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,9 +17,11 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LupaPasswordActivity extends AppCompatActivity {
+    private String TAG=getClass().getSimpleName();
     private EditText etemailkonfirmasi;
     private Button btlupakon;
     private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,23 +29,27 @@ public class LupaPasswordActivity extends AppCompatActivity {
         setContentView(R.layout.activity_lupa_password);
         etemailkonfirmasi = findViewById(R.id.etemailkonfirmasi);
         btlupakon= findViewById(R.id.btlupakonfirmasi);
+        firebaseAuth=FirebaseAuth.getInstance();
+
         btlupakon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String useremail = etemailkonfirmasi.getText().toString().trim();
+                final String useremail = etemailkonfirmasi.getText().toString().trim();
 
-                if (useremail.equals("")){
-                    Toast.makeText(LupaPasswordActivity.this, "Silahkan Masukkan Email Dancok !!!!", Toast.LENGTH_SHORT).show();
+                if (TextUtils.isEmpty(useremail)){
+                    Toast.makeText(LupaPasswordActivity.this, "Silahkan Masukkan Email !!", Toast.LENGTH_SHORT).show();
                 }else{
                     firebaseAuth.sendPasswordResetEmail(useremail).addOnCompleteListener(new OnCompleteListener<Void>() {
                         @Override
                         public void onComplete(@NonNull Task<Void> task) {
                             if (task.isSuccessful()){
-                                Toast.makeText(LupaPasswordActivity.this, "Password Reset Email Telah Terkirim", Toast.LENGTH_SHORT).show();
-                                finish();
-                                startActivity(new Intent(LupaPasswordActivity.this,MainActivity.class));
+                               // progressDialog.dismiss();
+                                Toast.makeText(LupaPasswordActivity.this, "Silahkan Cek Email untuk reset password", Toast.LENGTH_SHORT).show();
+                                Log.e(TAG, "Email: "+useremail);
                             }else{
+                                //progressDialog.dismiss();
                                 Toast.makeText(LupaPasswordActivity.this, "Error saat mengirim ke email, harap coba kembali", Toast.LENGTH_SHORT).show();
+                                Log.e(TAG, "Email: "+useremail);
                             }
                         }
                     });
--- app/src/main/java/com/imamabdulazis/loginfirebase/MainActivity.java
+++ app/src/main/java/com/imamabdulazis/loginfirebase/MainActivity.java
@@ -1,13 +1,36 @@
 package com.imamabdulazis.loginfirebase;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText Name;
+    private EditText Password;
+    private TextView Info;
+    private Button Login;
+    private int counter = 5;
+    private TextView userRegistration;
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
+    TextView tvlupapasword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvlupapasword=findViewById(R.id.tvlupapassword);
+        tvlupapasword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,LupaPasswordActivity.class));
+            }
+        });
     }
 }
